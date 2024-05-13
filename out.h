8305d7e7a982b9365ad0f779f98162a6e4e870b6
oguz-ismail <oguzismailuysal@gmail.com>
2024-05-13 21:19:59 +0300

    abandon stdio functions

---
+++ out.h
@@ -0,0 +1,11 @@
+#define OUT 0
+#define ERR 1
+
+#define outs(s) print_string(OUT, s)
+#define outn(x) print_number(OUT, x)
+#define errs(s) print_string(ERR, s)
+#define errn(x) print_string(ERR, x)
+
+void print_string(int, const char *);
+void print_number(int, int);
+void flush(int);
