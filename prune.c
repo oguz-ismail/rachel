5dda87687ec7dc915b35f082ff2c3f5fe2af8beb
oguz-ismail <oguzismailuysal@gmail.com>
2024-05-15 20:46:50 +0300

    prune more branches

--- prune.c
+++ prune.c
@@ -43,44 +43,41 @@ prune(unsigned ops) {
 		return ops;
 	}
 
-	if (rt != LEAF) {
-		switch (rt) {
-		case ADD:
-		case SUB:
-			ops &= ~(ADD|SUB);
-			break;
-		case MUL:
-		case DIV:
-			ops &= ~(MUL|DIV);
-			break;
-		}
+	switch (rt) {
+	case ADD:
+	case SUB:
+		ops &= ~(ADD|SUB);
+		break;
+	case MUL:
+	case DIV:
+		ops &= ~(MUL|DIV);
+		break;
 	}
-	else if (lt != LEAF) {
-		switch (lt) {
-		case SUB:
-			ops &= ~ADD;
-			break;
-		case DIV:
-			ops &= ~MUL;
-			break;
-		}
 
-		switch (lt) {
-		case ADD:
-		case SUB:
-		case MUL:
-		case DIV:
-			if (l->right->value < y)
-				ops &= ~lt;
+	switch (lt) {
+	case SUB:
+		ops &= ~ADD;
+		break;
+	case DIV:
+		ops &= ~MUL;
+		break;
+	}
 
-			break;
-		}
+	switch (lt) {
+	case ADD:
+	case SUB:
+	case MUL:
+	case DIV:
+		if (ops&lt && l->right->value < y)
+			ops &= ~lt;
+
+		break;
 	}
 
 	if (y == 1)
 		ops &= ~(MUL|DIV);
 
-	if (ops&SUB && x-y == y)
+	if (x-y == y)
 		ops &= ~SUB;
 
 	if (ops&DIV && x == y*y)
