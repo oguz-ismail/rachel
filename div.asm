0b521b834da9bfdccfb7057c9c4126ffea8c2c8a
oguz-ismail <oguzismailuysal@gmail.com>
2024-11-07 21:41:32 +0300

    support OpenBSD 7.6

--- div.asm
+++ div.asm
@@ -13,86 +13,86 @@
  * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-.global __aeabi_idiv
-.global __aeabi_idivmod
-.global __aeabi_uidiv
-.global __aeabi_uidivmod
+	.global	__aeabi_idiv
+	.global	__aeabi_idivmod
+	.global	__aeabi_uidiv
+	.global	__aeabi_uidivmod
 
-.text
+	.text
 
 __aeabi_idiv:
-	cmp r0, #0
-	bge .Lnum_pos
-	rsbs r0, r0, #0
-	cmp r1, #0
-	bge .Lneg_result
-	rsbs r1, r1, #0
-	b __aeabi_uidivmod
+	cmp	r0, #0
+	bge	.Lnum_pos
+	rsbs	r0, r0, #0
+	cmp	r1, #0
+	bge	.Lneg_result
+	rsbs	r1, r1, #0
+	b	__aeabi_uidivmod
 .Lnum_pos:
-	cmp r1, #0
-	bge __aeabi_uidivmod
-	rsbs r1, r1, #0
+	cmp	r1, #0
+	bge	__aeabi_uidivmod
+	rsbs	r1, r1, #0
 .Lneg_result:
-	push {lr}
-	bl __aeabi_uidivmod
-	rsbs r0, r0, #0
-	pop {pc}
+	push	{lr}
+	bl	__aeabi_uidivmod
+	rsbs	r0, r0, #0
+	pop	{pc}
 
 __aeabi_idivmod:
-	cmp r0, #0
-	bge .Lnum_pos_bis
-	rsbs r0, r0, #0
-	cmp r1, #0
-	bge .Lneg_both
-	rsbs r1, r1, #0
-	push {lr}
-	bl __aeabi_uidivmod
-	rsbs r1, r1, #0
-	pop {pc}
+	cmp	r0, #0
+	bge	.Lnum_pos_bis
+	rsbs	r0, r0, #0
+	cmp	r1, #0
+	bge	.Lneg_both
+	rsbs	r1, r1, #0
+	push	{lr}
+	bl	__aeabi_uidivmod
+	rsbs	r1, r1, #0
+	pop	{pc}
 .Lneg_both:
-	push {lr}
-	bl __aeabi_uidivmod
-	rsbs r0, r0, #0
-	rsbs r1, r1, #0
-	pop {pc}
+	push	{lr}
+	bl	__aeabi_uidivmod
+	rsbs	r0, r0, #0
+	rsbs	r1, r1, #0
+	pop	{pc}
 .Lnum_pos_bis:
-	cmp r1, #0
-	bge __aeabi_uidivmod
-	rsbs r1, r1, #0
-	push {lr}
-	bl __aeabi_uidivmod
-	rsbs r0, r0, #0
-	pop {pc}
+	cmp	r1, #0
+	bge	__aeabi_uidivmod
+	rsbs	r1, r1, #0
+	push	{lr}
+	bl	__aeabi_uidivmod
+	rsbs	r0, r0, #0
+	pop	{pc}
 
 __aeabi_uidiv:
 __aeabi_uidivmod:
-	cmp r1, #0
-	bne .Lno_div0
-	b __aeabi_idiv0
+	cmp	r1, #0
+	bne	.Lno_div0
+	b	__aeabi_idiv0
 .Lno_div0:
-	movs r2, #1
-	movs r3, #0
-	cmp r0, r1
-	bls .Lsub_loop0
-	adds r1, #0
-	bmi .Lsub_loop0
+	movs	r2, #1
+	movs	r3, #0
+	cmp	r0, r1
+	bls	.Lsub_loop0
+	adds	r1, #0
+	bmi	.Lsub_loop0
 .Ldenom_shift_loop:
-	lsls r2, #1
-	lsls r1, #1
-	bmi .Lsub_loop0
-	cmp r0, r1
-	bhi .Ldenom_shift_loop
+	lsls	r2, #1
+	lsls	r1, #1
+	bmi	.Lsub_loop0
+	cmp	r0, r1
+	bhi	.Ldenom_shift_loop
 .Lsub_loop0:
-	cmp r0, r1
-	bcc .Ldont_sub0
-	subs r0, r1
-	orrs r3, r2
+	cmp	r0, r1
+	bcc	.Ldont_sub0
+	subs	r0, r1
+	orrs	r3, r2
 .Ldont_sub0:
-	lsrs r1, #1
-	lsrs r2, #1
-	bne .Lsub_loop0
-	mov r1, r0
-	mov r0, r3
-	bx lr
+	lsrs	r1, #1
+	lsrs	r2, #1
+	bne	.Lsub_loop0
+	mov	r1, r0
+	mov	r0, r3
+	bx	lr
 __aeabi_idiv0:
-	bl __aeabi_idiv0
+	bl	__aeabi_idiv0
